4d08ea0141fecb110161e1cfe977d23db51299d9
Marcos Fernando Barbosa <marcosfba.algar@gmail.com>
2020-08-25 16:18:05 -0300

    Commit referente ao curso de Java Script da Dio

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function clicou(){
+    alert("Obrigado por clicar");
+}
+
+
+
+
+//var nome ="Marcos Fernando Barbosa";
+//var idade =39;
+//alert("Bem vindo " + nome + " sua idade é:" + idade);
+
+//console.log(nome);
+//console.log(idade++);
+
+//var lista = ["maça","pera","uva"];
+//lista.push("uva rubi");
+//lista.pop();
+//console.log(lista);
+
+//var fruta = {nome:"maça", cor:"vermelha"};
+//console.log(fruta.nome)
+
+
